98c0cb7cf472a3f20c97b3f863cfb17cacbbdbe3
Seagull2004 <91527519+Seagull2004@users.noreply.github.com>
2022-11-12 10:37:12 +0100

    Versione 6.0
    Aggiunta di funzioni che vengono richiamate nella pagina post_media al cambiamento delle opzioni su Regione, Provincia, Comune e Scuola

---
+++ Compax/php/config.php
@@ -0,0 +1,22 @@
+<?php
+$serverName = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "compax_software";
+
+$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
+
+if(!$conn){
+    die("Connessione fallita: " . mysqli_connect_error());
+}
+
+
+$sqlRegioni = "SELECT DISTINCT Regione FROM scuole ORDER BY `scuole`.`Regione` ASC";
+$sqlProvincia = "SELECT DISTINCT Provincia FROM scuole ORDER BY `scuole`.`Provincia` ASC";
+$sqlComune = "SELECT DISTINCT Comune FROM scuole WHERE Regione = 'Piemonte' ORDER BY `scuole`.`Comune` ASC;";
+$sqlNomeIstituto = "SELECT DISTINCT `Nome istituto` FROM scuole";
+
+$regioni = mysqli_query($conn, $sqlRegioni);
+$comuni = mysqli_query($conn, $sqlComune);
+$provincie = mysqli_query($conn, $sqlProvincia);
+$scuole = mysqli_query($conn, $sqlNomeIstituto);
